f685ffba362711b05acb44aa705dba96c4a6f4f6
aadrikasingh18 <aadrikas16@gmail.com>
2024-02-13 23:20:56 +0530

    Changes...

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const { poolcb } = require("./dbConfig.js");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 //MIDDLEWARE
 app.use(express.json());
